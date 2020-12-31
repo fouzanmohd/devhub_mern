a3cfa5366952c315dba6d47fc10a85b899f0c1bf
Muhammed Fouzan <fouzanpharmd@gmail.com>
2021-01-01 00:39:09 +0530

    connected mongodb using mongoose

--- config/db.js
+++ config/db.js
@@ -1,4 +1,19 @@
-const mongoose = require('mongoose')
-const config = require('config')
+const mongoose = require("mongoose");
+const config = require("config");
 
-const db = config.get('mongoURI')
+const db = config.get("mongoURI", {});
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB database successfully!");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const connectDB = require('./config/db')
 
 app.get('/', (req,res)=>{
     res.send('Api is running!')
 })
+
+// MongoDB Connection
+connectDB();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}`))
