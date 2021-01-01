eaf9fe46333c0f9866e26f9801484cde37ccdf14
Muhammed Fouzan <fouzanpharmd@gmail.com>
2021-01-01 23:03:02 +0530

    user model and user registration config done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 //@route POST api/users
 router.post(
@@ -12,12 +15,43 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User Route");
+
+    const { name, email, password } = req.body;
+    try{
+      //Check duplication of email
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(500).json({ errors: [{ message: "User already exist" }] });
+    }
+
+    //gravatar setting
+    const avatar = gravatar.url(email, {
+      s:'200',
+      r: 'pg',
+      d: 'mm'
+    })
+    //creating User model instance 
+     user = new User({
+      name, email, password, avatar
+    })
+
+    //Encrypting password
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(password, salt) 
+
+    await user.save()
+
+    res.send("User Created Successfully!");
+    } catch(err){
+      console.error({Error: err.message})
+      res.status(500).send("Server error!")
+    }
+    
   }
 );
 
