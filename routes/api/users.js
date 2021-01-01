5cba44f87bf0d62d2a055f78bf5c35779cdb068f
Muhammed Fouzan <fouzanpharmd@gmail.com>
2021-01-02 00:20:00 +0530

    added jwt token

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
 const bcrypt = require("bcryptjs");
 const gravatar = require("gravatar");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 //@route POST api/users
 router.post(
@@ -22,36 +24,53 @@ router.post(
     }
 
     const { name, email, password } = req.body;
-    try{
+    try {
       //Check duplication of email
-    let user = await User.findOne({ email });
-    if (user) {
-      return res.status(500).json({ errors: [{ message: "User already exist" }] });
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(500)
+          .json({ errors: [{ message: "User already exist" }] });
+      }
+
+      //gravatar setting
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+      //creating User model instance
+      user = new User({
+        name,
+        email,
+        password,
+        avatar,
+      });
+
+      //Encrypting password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error({ Error: err.message });
+      res.status(500).send("Server error!");
     }
-
-    //gravatar setting
-    const avatar = gravatar.url(email, {
-      s:'200',
-      r: 'pg',
-      d: 'mm'
-    })
-    //creating User model instance 
-     user = new User({
-      name, email, password, avatar
-    })
-
-    //Encrypting password
-    const salt = await bcrypt.genSalt(10)
-    user.password = await bcrypt.hash(password, salt) 
-
-    await user.save()
-
-    res.send("User Created Successfully!");
-    } catch(err){
-      console.error({Error: err.message})
-      res.status(500).send("Server error!")
-    }
-    
   }
 );
 
